2a40df1612254bf3b5df6d63736d9fd1524d7bad
James Yeates <jimibue@gmail.com>
2020-11-23 11:55:00 -0700

    create account working

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,6 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
+
   def index
     @accounts = current_user.accounts
   end
@@ -7,8 +9,30 @@ class AccountsController < ApplicationController
   end
 
   def new
+    @account = Account.new
+  end
+
+  def create
+    # one way to create an account does not santize
+    # current_user.accounts.create(name: params[:account][:name], balance: params[:account][:balance])
+    account = current_user.accounts.new(account_params)
+    if account.save
+      redirect_to accounts_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def set_account
+    @account = current_user.accounts.find(params[:id])
+  end
+
+  def account_params
+    params.require(:account).permit(:name, :balance)
   end
 end
