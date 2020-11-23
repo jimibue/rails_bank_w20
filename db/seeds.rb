6b18fa8da9dc75b3a713d1b753a0209e11b4da0e
James Yeates <jimibue@gmail.com>
2020-11-23 11:28:12 -0700

    account index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(first_name: "John", last_name: "Doe", email: "test3@test.com", password: "123456")
+
+user1.accounts.create(name: "Checking", balance: 29.95)
+user1.accounts.create(name: "Saving", balance: 2229.95)
+user1.accounts.create(name: "Fun", balance: 9123234.95)
